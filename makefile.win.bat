c7cf12c887a8eb4fdc5c360ce779824e28bd6c2e
paoli612 <paoli7612@gmail.com>
2021-01-31 11:42:21 +0100

    eh no eh

--- makefile.win.bat
+++ makefile.win.bat
@@ -1,3 +1,3 @@
 g++ ./client/main.cpp -static-libgcc -lwsock32 -o ./client/main.exe
-g++ ./server/main.cpp -static-libgcc -lwsock32 -o ./client/main.exe
-g++ ./launcher/main.cpp -static-libgcc -lwsock32 -o ./client/main.exe
+g++ ./server/main.cpp -static-libgcc -lwsock32 -o ./server/main.exe
+g++ ./launcher/main.cpp -static-libgcc -lwsock32 -o ./launcher/main.exe
